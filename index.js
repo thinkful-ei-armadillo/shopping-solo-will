6ea7157a9268d858bb4bdd31ad6e8df68c7cb53c
William Y Wong <wwong1987@gmail.com>
2019-01-19 22:21:06 -0800

    added search functionality with shopping list re-rendering on empty search input submission

--- index.js
+++ index.js
@@ -28,25 +28,12 @@ function generateItemElement(item, itemIndex, template) {
     </li>`;
 }
 
-
 function generateShoppingItemsString(shoppingList) {
   const items = shoppingList.map((item, index) => generateItemElement(item, index));
   return items.join('');
 }
 
-function toggleCheckedItem() {
-  STORE.hideCompleted = !STORE.hideCompleted;
-}
-
-function handleToggleCheckedItem() {
-  $('#js-hide-completed-checkbox').on('click', event => {
-    console.log('`handleToggleCheckedItem` ran');
-    toggleCheckedItem();
-    renderShoppingList();
-  });
-}
-
-function renderShoppingList() {
+function renderShoppingList(shoppingList = STORE.items) { // set default parameter to STORE.items
   // render the shopping list in the DOM
   console.log('`renderShoppingList` ran');
   if (STORE.hideCompleted) {
@@ -55,15 +42,24 @@ function renderShoppingList() {
     $('.js-shopping-list').html(shoppingListItemsString);
   } else {
   // if else condition to filter out the items that are checked 
-    let shoppingListItemsString = generateShoppingItemsString(STORE.items);
+    let shoppingListItemsString = generateShoppingItemsString(shoppingList);
     // insert that HTML into the DOM
     $('.js-shopping-list').html(shoppingListItemsString);
   }
-  // ----- alternative with ternary ------
-  // const shoppingListItemsString = STORE.hideCompleted ? generateShoppingItemsString(STORE.items.filter(item => !item.checked)) : generateShoppingItemsString(STORE.items);
-  // $('.js-shopping-list').html(shoppingListItemsString);
 }
 
+function getItemIndexFromElement(item) {
+  const itemIndexString = $(item)
+    .closest('.js-item-index-element')
+    .attr('data-item-index');
+  return parseInt(itemIndexString, 10);
+}
+
+// ----- alternative with ternary ------
+// const shoppingListItemsString = STORE.hideCompleted ? generateShoppingItemsString(STORE.items.filter(item => !item.checked)) : generateShoppingItemsString(STORE.items);
+// $('.js-shopping-list').html(shoppingListItemsString);
+
+// ================ ADD ITEM FUNCTIONALITY ===========================
 
 function addItemToShoppingList(itemName) {
   console.log(`Adding "${itemName}" to shopping list`);
@@ -81,18 +77,11 @@ function handleNewItemSubmit() {
   });
 }
 
+// ================ CHECKING ITEM FUNCTIONALITY ===========================
 
 function toggleCheckedForListItem(itemIndex) {
   console.log('Toggling checked property for item at index ' + itemIndex);
   STORE.items[itemIndex].checked = !STORE.items[itemIndex].checked;
-}
-
-
-function getItemIndexFromElement(item) {
-  const itemIndexString = $(item)
-    .closest('.js-item-index-element')
-    .attr('data-item-index');
-  return parseInt(itemIndexString, 10);
 }
 
 function handleItemCheckClicked() {
@@ -104,6 +93,21 @@ function handleItemCheckClicked() {
   });
 }
 
+// ================ TOGGLE ALL CHECKED FUNCTIONALITY ===========================
+
+function toggleCheckedItem() {
+  STORE.hideCompleted = !STORE.hideCompleted;
+}
+
+function handleToggleCheckedItem() {
+  $('#js-hide-completed-checkbox').on('click', event => {
+    console.log('`handleToggleCheckedItem` ran');
+    toggleCheckedItem();
+    renderShoppingList();
+  });
+}
+
+// ====================== DELETE FUNCTIONALITY ===========================
 
 function deleteItemFromList(itemIndex) {
   console.log('Delete item at index ' + itemIndex);
@@ -120,21 +124,34 @@ function handleDeleteItemClicked() {
   });
 }
 
+// ====================== SEARCH FUNCTIONALITY ===========================
+
+// create function to iterate through and filter STORE.items for the value of user input grabbed from search value. Maybe use find() or filter()?
+function filterSearchItem(input) {
+  return STORE.items.filter(item => item.name === input);
+}
+
 // create function to grab value of user input (similar to handleNewItemSubmit)
 function handleNewSearchItem() {
-  $('#js-shopping-list-form').submit(function(event) {
+  $('#js-search-form').submit(function(event) {
     event.preventDefault();
-    console.log('handleNewSearchItem ran');
-    const newSearchItem = $('.js-search-entry').val();
+    const searchInput = $('.js-search-entry').val();
+    const searchResults = filterSearchItem(searchInput);
+    console.log(searchInput);
+    console.log(searchResults);
     $('.js-search-entry').val('');
-    // callback function for filtering name
-    // renderShoppingList();
+    // filterSearchItem(searchValue);
+    // console.log(filterSearchItem(searchValue));
+    if (searchResults.length < 1) { // if search result input is empty on submit click then render normal shopping list without search result filter
+      renderShoppingList();
+    } else {
+      renderShoppingList(searchResults);
+    }
   });
 }
 
 
-// create function to iterate through and filter STORE.items for the value of user input grabbed from search value
-// maybe use hide completed implementation?
+// ====================== EDIT FUNCTIONALITY ===========================
 
 
 
@@ -142,6 +159,16 @@ function handleNewSearchItem() {
 
 
 
+
+
+// if (STORE.items) {
+//   const filteredSearchItem = STORE.items.filter(item => !item.name);
+//   let searchResultStrings = generateShoppingItemsString(filteredSearchItem);
+//   $('.js-shopping-list').html(searchResultStrings);
+// } else {
+//   let searchResultStrings = generateShoppingItemsString(searchResultStrings);
+//   $('.js-shopping-list').html(searchResultStrings);
+// }
 
 // this function will be our callback when the page loads. it's responsible for
 // initially rendering the shopping list, and activating our individual functions
@@ -153,27 +180,9 @@ function handleShoppingList() {
   handleItemCheckClicked();
   handleDeleteItemClicked();
   handleToggleCheckedItem();
-  // handleNewSearchItem();
+  handleNewSearchItem();
 }
 
 
 // when the page loads, call `handleShoppingList`
 $(handleShoppingList);
-console.log('testing');
-
-/*
-
-function renderShoppingList() {
-  // this function will be responsible for rendering the shopping list in
-  // the DOM
-  const html = STORE.item.map((item, index) => generateHtml(item, index)).join('');
-  console.log('`renderShoppingList` ran');
-  // let filteredItems = [...STORE.items];
-  // if (STORE.hideCompleted) {
-    filteredItems = filteredItems.filter(item => {
-      items.checked === false;
-    })
-  }
-  return $('.js-shopping-list').html(html);
-}
-*/
